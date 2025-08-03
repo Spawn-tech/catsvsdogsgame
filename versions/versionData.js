42a124dd493272993edffe5f97a666e3100e3451
Spawntech <ryansantiago135@gmail.com>
2025-08-03 13:19:46 -0400

    Update versionData.js

--- versions/versionData.js
+++ versions/versionData.js
@@ -19,18 +19,20 @@ var verData = [
     ["Full release", "moar levels", "cool stuff"],
   ],
   ["0.2", "March 28, 2025", "apoeifja903-j2r-0293rj", ["Bug fixes"]],
-  ["0.1", "March 27, 2025", "fjaslkdjfaps", ["Demo release"]],
+  ["0.0", "May 30, 2025", "2RZjqfGg85Kp7HYduICk0LA3Ij5sI5hj-yexZdzFsLg", ["Initial demo release"]],
 ];
 var formattedData = "";
 for (var i = 0; i < verData.length; i++) {
   formattedData +=
     '<div class="sectionBox"><iframe class="button" align="left" width="150" height="150" src="../animated/smallplaybutton.html?i=' +
     verData[i][2] +
-    '"></iframe><h6>v' +
+    '"></iframe><h6><a href="https://studio.code.org/projects/gamelab/"' +
+    verData[i][2] +
+    '/embed?nosource">v' +
     verData[i][0] +
     " - Released " +
     verData[i][1] +
-    '</h6><div class="notes"><p>- ' +
+    '</a></h6><div class="notes"><p>- ' +
     verData[i][3].join("<br>- ") +
     "</p></div></div>";
 }
